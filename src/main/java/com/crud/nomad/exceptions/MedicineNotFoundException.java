285697e73421c18f34c1ec0a8a12274505a025eb
ZuzannJanik <zjanik89@gmail.com>
2023-10-08 14:02:09 +0800

    Adding methods to exceptions

--- src/main/java/com/crud/nomad/exceptions/MedicineNotFoundException.java
+++ src/main/java/com/crud/nomad/exceptions/MedicineNotFoundException.java
@@ -1,4 +1,7 @@
 package com.crud.nomad.exceptions;
 
 public class MedicineNotFoundException extends Throwable {
+    public MedicineNotFoundException() {
+        super("Medicine id does not exist. Check the correctness of the data.");
+    }
 }
