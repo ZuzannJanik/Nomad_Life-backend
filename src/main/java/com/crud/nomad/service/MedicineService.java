adca5119a8c8d7d27fc5996b0fd6673f511bcc89
ZuzannJanik <zjanik89@gmail.com>
2023-10-12 09:06:47 +0800

    Adding services tests

--- src/main/java/com/crud/nomad/service/MedicineService.java
+++ src/main/java/com/crud/nomad/service/MedicineService.java
@@ -10,22 +10,19 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
-
 public class MedicineService {
+
     private final MedicineRepository repository;
     public List<Medicine> getAllMedicines() {
-
         return repository.findAll();
     }
     public Medicine saveMedicine(final Medicine medicine) {
-
         return repository.save(medicine);
     }
     public Medicine getMedicine(final Long medicineId) throws MedicineNotFoundException {
         return repository.findById(medicineId).orElseThrow(MedicineNotFoundException::new);
     }
     public void deleteMedicine(final Long medicineId) {
-
         repository.deleteById(medicineId);
     }
 }
